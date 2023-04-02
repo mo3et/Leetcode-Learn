334e9bfb7f2c091ae067649b31440f3b4c7e8560
Monet Lee <monet_lee@163.com>
2023-04-02 16:49:58 +0800

    update

--- Leetcode-Go/leetcode/0001.Two-Sum/1.Two-Sum.go
+++ Leetcode-Go/leetcode/0001.Two-Sum/1.Two-Sum.go
@@ -1,7 +1,8 @@
 //
 
-// package leetcode
-package main
+package leetcode
+
+// package main
 
 import "fmt"
 
@@ -10,15 +11,17 @@ import "fmt"
 // )
 
 func twoSum(num []int, target int) []int {
-	m := make(map[int]int) // 0 x1, 1 x2, 2 x3, 3 x4
-	fmt.Println(m)
+	m := make(map[int]int)  // 0 x1, 1 x2, 2 x3, 3 x4
 	for k, v := range num { //k is i, v is num[i]
+		fmt.Println(m)
 		// fmt.Println(k, v)
 		// fmt.Println("Its ", m[target-v], "and m[k]", m[k], " and m[v]", m[v])
 
 		// m[target-v] // == m[another]  another:=target-num[i]
+		fmt.Println(m[v])
 
 		if another, ok := m[target-v]; ok { //idx is target - value, or name another
+			fmt.Println([]int{k, v})
 			fmt.Println([]int{another, k})
 			return []int{another, k}
 		}
@@ -42,9 +45,24 @@ func twoSum(num []int, target int) []int {
 // 	return nil
 // }
 
-func main() {
+// ============= Solve2 ===================
+//  Use range
+// func twoSum(nums []int, target int) []int {
+// 	m := make(map[int]int)
+// 	for k, v := range nums {
+// 		if idx, ok := m[target-v]; ok {
+// 			return []int{idx, k}
+// 		}
+// 		m[v] = k
+// 	}
+// 	return nil
+// }
 
-	para1 := []int{0, 8, 7, 3, 3, 4, 2}
-	twoSum(para1, 11)
-	// ans1{[]int{1, 3}},
-}
+// Go run test (No )
+
+// func main() {
+
+// 	para1 := []int{0, 8, 7, 3, 3, 4, 2}
+// 	twoSum(para1, 11)
+// 	// ans1{[]int{1, 3}},
+// }
