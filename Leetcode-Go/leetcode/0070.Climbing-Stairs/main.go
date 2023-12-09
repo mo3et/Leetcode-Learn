9a072df12f91c509532b35b9d0d1d5917e91693b
Monet Lee <monet_lee@163.com>
2023-12-10 02:12:25 +0800

    update

--- Leetcode-Go/leetcode/0070.Climbing-Stairs/main.go
+++ Leetcode-Go/leetcode/0070.Climbing-Stairs/main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+// F(n)=F(n-1)+F(n-2)
+// 先递归，然后记忆化，最后滚动数组
+
 func main() {
 	x := climbStairs1(9)
+	res := climbStairs2(9)
 	fmt.Println(x)
+	fmt.Println("ans is", res)
 }
 
+// DP 滚动数组实现
 func climbStairs1(n int) int {
 	q, p, r := 0, 0, 1
 	for n > 0 {
@@ -16,4 +22,16 @@ func climbStairs1(n int) int {
 		n--
 	}
 	return r
+}
+
+func climbStairs2(n int) int {
+	// 跳过0，直接从 index=2 开始
+	d1, d2 := 1, 1
+	for i := 2; i <= n; i++ {
+		// d2(Fn)= d2(old d2) +d1
+		// d1(F(n-1))= d2(old d2)
+
+		d1, d2 = d2, d2+d1
+	}
+	return d2
 }
--- Leetcode-Go/leetcode/0206.Reverse-Linked-List/main.go
+++ Leetcode-Go/leetcode/0206.Reverse-Linked-List/main.go
@@ -1,28 +1,32 @@
-package main
+package leetcode
 
 import (
-	"fmt"
-
 	"github.com/mo3et/leetcode-learn/leetcode-go/structures"
 )
 
 type ListNode = structures.ListNode
 
-func main() {
-	head := &ListNode{
-		Val: 1,
-		Next: &ListNode{
-			Val: 2,
-			Next: &ListNode{
-				Val:  3,
-				Next: nil,
-			},
-		},
-	}
-	res := reverseList(head)
-	res = ListNode(res)
-	fmt.Println("ans is ", res)
-}
+// func main() {
+// 	head := &ListNode{
+// 		Val: 1,
+// 		Next: &ListNode{
+// 			Val: 2,
+// 			Next: &ListNode{
+// 				Val:  3,
+// 				Next: nil,
+// 			},
+// 		},
+// 	}
+// 	res := reverseList(head)
+
+// 	resV := structures.List2Ints(reverseList(structures.Ints2List(resS)))
+// 	resV := make([]int, 0)
+// 	for i := 0; i < len(&res); i++ {
+// 		resV
+// 	}
+
+// 	fmt.Println("ans is ", resV)
+// }
 
 func reverseList(head *ListNode) *ListNode {
 	var behind *ListNode
